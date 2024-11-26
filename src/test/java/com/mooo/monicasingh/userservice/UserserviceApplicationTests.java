e4fd849835ce1283bcd9c7d1486c8bb69b48e94d
Waquar Shamsi <waquarshamsi@outlook.com>
2024-11-27 04:57:33 +0530

    initial project setup

---
+++ src/test/java/com/mooo/monicasingh/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooo.monicasingh.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
